55b58128f4b21161c91fabc6fd3d15ec4f37fabb
bhawiyuga <adhitya.bhawiyuga@gmail.com>
2024-05-20 20:43:39 +0200

    playbook config monitoring copy dataset

--- rs-exporter/build.sh
+++ rs-exporter/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
-cross build --target x86_64-unknown-linux-gnu --release
+cargo build --release --bin rs-exporter
+cross build --target x86_64-unknown-linux-gnu --release --bin rs-exporter
