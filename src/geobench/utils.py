40328f0246f25cae8ea39f0e258a096f3ab9be45
Serkan Girgin <girgink@gmail.com>
2025-08-21 11:59:01 +0200

    Removed unused code files

--- src/geobench/utils.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def create_directories(directories):
-    for directory in directories:
-        if not os.path.exists(directory):
-            os.makedirs(directory)
-            print(f"Directory {directory} created.")
-        else:
-            print(f"Directory {directory} already exists.")
