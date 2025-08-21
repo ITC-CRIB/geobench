e69e2d0c263e4fd1e951ee4359a114a3e354c2d6
Serkan Girgin <girgink@gmail.com>
2025-08-21 16:01:24 +0200

    Converted filename to a positional argument

--- src/geobench/main.py
+++ src/geobench/main.py
@@ -9,7 +9,7 @@ from .scenario import load_scenario
 class CommandLineTool:
     def __init__(self):
         self.parser = argparse.ArgumentParser(description="Benchmarking toolkit for geospatial processing workflows.")
-        self.parser.add_argument('-f', '--file', type=str, help='Scenario file name.')
+        self.parser.add_argument('filename', type=str, help='Scenario filename.')
         self.parser.add_argument('-n','--name', type=str, help='Scenario name.')
         self.parser.add_argument('-r', '--repeat', type=int, help='Number of repeats.', default=1)
 
@@ -17,21 +17,17 @@ class CommandLineTool:
     def run(self):
         args = self.parser.parse_args()
 
-        if args.file:
-            path = os.path.abspath(args.file)
+        path = os.path.abspath(args.filename)
 
-            try:
-                scenario = load_scenario(path, args)
+        try:
+            scenario = load_scenario(path, args)
 
-                benchmark = Benchmark(scenario)
-                benchmark.run()
+            benchmark = Benchmark(scenario)
+            benchmark.run()
 
-            except Exception as err:
-                print(err)
-                sys.exit(1)
-
-        else:
-            self.parser.print_help()
+        except Exception as err:
+            print(err)
+            sys.exit(1)
 
 
 def main():
