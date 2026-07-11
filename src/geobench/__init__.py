d1b7c20e1fffbcf5174f0cf8c9034a7ead468f59
Serkan Girgin <girgink@gmail.com>
2026-07-11 09:53:18 +0200

    Relocated geobench decorator

--- src/geobench/__init__.py
+++ src/geobench/__init__.py
@@ -2,6 +2,28 @@
 GeoBench - A benchmarking tool for geospatial operations.
 """
 
-from .jupyter import Geobench, geobench
+from .jupyter import Geobench
 
 __all__ = ["Geobench", "geobench"]
+
+
+def geobench(name: str | None = None, **kwargs) -> callable:
+    """Create a decorator that benchmarks the execution of a function.
+
+    Args:
+        name: Optional name of the benchmark. If None, the name of the decorated function is used.
+        **kwargs: Additional keyword arguments passed to the Geobench constructor.
+
+    Returns:
+        A decorator that wraps the function and benchmarks its execution.
+    """
+
+    def decorator(func):
+        bench = Geobench(name or func.__name__, **kwargs)
+
+        def wrapper(*args, **kwargs):
+            return bench.benchmark(func, *args, **kwargs)
+
+        return wrapper
+
+    return decorator
--- src/geobench/jupyter.py
+++ src/geobench/jupyter.py
@@ -339,25 +339,3 @@ class Geobench:
         )
 
         return report_path
-
-
-def geobench(name: str | None = None, **kwargs) -> callable:
-    """Create a decorator that benchmarks the execution of a function.
-
-    Args:
-        name: Optional name of the benchmark. If None, the name of the decorated function is used.
-        **kwargs: Additional keyword arguments passed to the Geobench constructor.
-
-    Returns:
-        A decorator that wraps the function and benchmarks its execution.
-    """
-
-    def decorator(func):
-        bench = Geobench(name or func.__name__, **kwargs)
-
-        def wrapper(*args, **kwargs):
-            return bench.benchmark(func, *args, **kwargs)
-
-        return wrapper
-
-    return decorator
