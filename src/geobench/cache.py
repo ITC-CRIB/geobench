ad2ec614ed1a620f267c7ceb5c860d1071082b78
Serkan Girgin <girgink@gmail.com>
2026-08-02 18:04:08 +0200

    Improved OS handling

--- src/geobench/cache.py
+++ src/geobench/cache.py
@@ -1,50 +1,34 @@
 """Cache module."""
 
 import ctypes
-import os
-import platform
-
 import logging
+import os
 
 logger = logging.getLogger(__name__)
 
 
-def _clear_linux_cache():
-    """Clear system caches on Linux."""
-    logger.debug("Flushing all filesystem buffers to disk")
-    try:
-        os.system("sync")
-
-    except Exception as err:
-        logger.debug("Failed to flush filesystem buffers to disk: %s", err)
-
-    logger.debug("Clearing filesystem cache from memory")
-    try:
-        os.system("echo 3 > /proc/sys/vm/drop_caches")
-
-    except Exception as err:
-        logger.debug("Failed to clear filesystem cache from memory: %s", err)
-
-
-def _clear_windows_cache():
-    """Clear system caches on Windows."""
-    logger.debug("Trimming memory pages")
-    try:
-        ctypes.windll.psapi.EmptyWorkingSet(-1)
-
-    except Exception as err:
-        logger.debug("Failed to trim memory pages: %s", err)
-
-
 def clear_cache():
     """Clear system caches."""
-    system = platform.system()
+    # Try to flush all filesystem buffers to disk (Linux)
+    if hasattr(os, "sync"):
+        logger.debug("Flushing all filesystem buffers to disk")
+        os.sync()
 
-    if system == "Linux":
-        _clear_linux_cache()
+    # Try to drop caches (Linux)
+    try:
+        with open("/proc/sys/vm/drop_caches", "w") as file:
+            logger.debug("Dropping filesystem cache from memory")
+            file.write("3\n")
+    except FileNotFoundError:
+        pass
+    except (PermissionError, OSError) as err:
+        logger.debug("Failed to drop caches: %s", err)
 
-    elif system == "Windows":
-        _clear_windows_cache()
-
-    else:
-        logger.debug("Unsupported operating system: %s", system)
+    # Try to trim memory pages (Windows)
+    if hasattr(ctypes, "windll"):
+        try:
+            psapi = ctypes.WinDLL("psapi", use_last_error=True)
+            if not psapi.EmptyWorkingSet(-1):
+                raise ctypes.WinError(ctypes.get_last_error())
+        except OSError as err:
+            logger.debug("Failed to trim memory pages: %s", err)
